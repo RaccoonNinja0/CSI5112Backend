c7ea43186c676bfc94bb46b44a2705c9b6219fc0
RaccoonNinja0 <97842065+RaccoonNinja0@users.noreply.github.com>
2022-03-20 00:02:25 -0400

    Update Chatbox.cs

--- Models/Chatbox.cs
+++ Models/Chatbox.cs
@@ -3,13 +3,13 @@ namespace csi5112service.models;
 public class Chatbox{
     public string PostId{get; set;}
     public string Question{get; set;}
-    public string ListOfReplies{get; set;}
+    public string Reply{get; set;}
     // public List<string> ListOfReplies = {get; set;}
     public string PostDate{get; set;}
-    public Chatbox(string PostId, string Question, string ListOfReplies, string PostDate){
+    public Chatbox(string PostId, string Question, string Reply, string PostDate){
         this.PostId = PostId;
         this.Question = Question;
-        this.ListOfReplies = ListOfReplies;
+        this.Reply = Reply;
         this.PostDate = PostDate;
     }
 }
