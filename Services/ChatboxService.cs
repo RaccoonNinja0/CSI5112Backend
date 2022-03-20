c73c3a5223b825998818870fe4cdcc308762c7ae
RaccoonNinja0 <97842065+RaccoonNinja0@users.noreply.github.com>
2022-03-20 00:05:19 -0400

    Update ChatboxService.cs

--- Services/ChatboxService.cs
+++ Services/ChatboxService.cs
@@ -4,9 +4,9 @@ namespace csi5112service.services;
 public class ChatboxService {
 	private List<Chatbox> Chatbox = new List<Chatbox>(){
 		new Chatbox("post001", "How can I track my order?", "You can track with your order number.", "01-Jan-2022"),
-		new Chatbox("post002", "What is the phone number of customer service?", "Please call 800-000-1234.", "02-March-2022"),
+		new Chatbox("post002", "What is the phone number of customer service?", "", "02-March-2022"),
 		new Chatbox("post003", "How long does the express mail usually arrive?", "It usually takes 5-7 business days.", "18-Feb-2022"),
-		new Chatbox("post004", "Can I return or exchange this item?", "Please contact our customer service.", "23-Feb-2022"),
+		new Chatbox("post004", "Can I return or exchange this item?", "Please call 800-000-1234.", "23-Feb-2022"),
 	};
 
 	public ChatboxService(){}
